ce0413acf105fd07dab60fa4409353ccd8e7e168
Andre M <andrepaesbm@gmail.com>
2022-10-15 22:22:43 +0000

    Added login and logout properties

--- app/actions.rb
+++ app/actions.rb
@@ -1,5 +1,13 @@
 # Controller - Handles client HTTP requests and sends HTTP responses 
 
+helpers do
+
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+
+end
+
 # Handle the GET request for '/' (Display all the Finstagram posts by descending order) - READ
 get '/' do
   @finstagram_posts = FinstagramPost.order(created_at: :desc)
@@ -28,11 +36,47 @@ post '/signup' do  #If a user navigates to the path "/signup"
   
   if @user.save
     
-    "User #{@user.username} created!"
+    redirect to('/login')
 
   else
     
     erb(:signup)
 
    end
+end
+
+get '/login' do  
+
+  erb(:login) 
+  
+end
+
+post '/login' do
+  
+  username = params[:username]
+  password = params[:password]
+
+  user = User.find_by(username: username)
+
+  if user && user.password == password
+
+    session[:user_id] = user.id
+
+      redirect to('/')
+
+  else
+
+    @error_message = "Login failed."
+
+    erb(:login)
+
+  end
+end
+
+get '/logout' do
+
+  session[:user_id] = nil
+
+  redirect to('/')
+
 end
